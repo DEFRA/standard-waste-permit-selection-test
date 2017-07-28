5f992c23432098ccddebda093b3d6bf48d90d2db
Martin Glancy <glancyea@gmail.com>
2017-07-28 17:00:17 +0100

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var router = express.Router()
+
+// anything beginning with "/vn" will go into this
+router.use('/v7', require('./routes_v7'));
+
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+module.exports = router
