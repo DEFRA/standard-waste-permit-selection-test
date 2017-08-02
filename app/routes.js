18e65fc491bad12ef82795a35d29fbf850502e67
Martin Glancy <glancyea@gmail.com>
2017-08-02 15:01:48 +0100

    Swap button style

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ router.use(function (req, res, next) {
   // set a folder and store in locals
   // this can then be used in pages as {{folder}}
   //res.locals.surveyURL="https://www.smartsurvey.co.uk/s/TELLUS/?surveydata="
-  res.locals.surveyURL="http://www.smartsurvey.co.uk/s/7Y0HE/?surveydata="
+  res.locals.surveyURL="http://www.smartsurvey.co.uk/s/7Y0HE/?"
   next()
 });
 
@@ -62,53 +62,44 @@ router.get('/permit', function (req, res) {
 
 // Found or error ==============================================================
 
+// VARS
+// back
+// oldcat
+// cat
+// permit
+// page
+
+function createURL(query){
+  return Object.keys(query).map(function(k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(query[k])
+  }).join('&')
+}
+
 router.get('/found', function (req, res) {
     req.query.page = 'found'
     res.render('found',{
-      "qS":encodeURI( JSON.stringify(req.query) )
+      "qS":createURL(req.query)
     })
 })
 
 router.get('/notfound', function (req, res) {
     req.query.page = 'notfound'
     res.render('notfound',{
-      "qS":encodeURI( JSON.stringify(req.query) )
+      "qS":createURL(req.query)
     })
 })
 
 router.get('/error', function (req, res) {
     req.query.page = 'error'
     res.render('error',{
-      "qS":encodeURI( JSON.stringify(req.query) )
+      "qS":createURL(req.query)
     })
 })
 
 router.get('/thanks', function (req, res) {
     req.query.page = 'thanks'
     res.render('thanks',{
-      "qS":encodeURI( JSON.stringify(req.query) )
-    })
-})
-
-
-
-
-
-
-
-
-// Permit search ===================================================================
-
-router.get('/search-permit/index', function (req, res) {
-    res.render(folder + '/search-permit/index',{
-      "formAction": "/search-permit/index"
-    })
-})
-
-router.post('/search-permit/index', function (req, res) {
-    res.render(folder + '/search-permit/index',{
-      "formAction":"/search-permit/index",
-      "searchTerm":req.body.searchTerm
+      "qS":createURL(req.query)
     })
 })
 
