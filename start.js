3ed86a85e9a477dfb943fd4635b164c39d58fa1d
Martin Glancy <martin.glancy@environment-agency.gov.uk>
2019-09-06 15:21:15 +0100

    Test commit 2

--- start.js
+++ start.js
@@ -23,11 +23,11 @@ if (!envExists) {
 var spawn = require('cross-spawn')
 
 process.env['FORCE_COLOR'] = 1
-var gulp = spawn('gulp')
-gulp.stdout.pipe(process.stdout)
-gulp.stderr.pipe(process.stderr)
-process.stdin.pipe(gulp.stdin)
-
-gulp.on('exit', function (code) {
-  console.log('gulp exited with code ' + code.toString())
-})
+// var gulp = spawn('gulp')
+// gulp.stdout.pipe(process.stdout)
+// gulp.stderr.pipe(process.stderr)
+// process.stdin.pipe(gulp.stdin)
+//
+// gulp.on('exit', function (code) {
+//   console.log('gulp exited with code ' + code.toString())
+// })
